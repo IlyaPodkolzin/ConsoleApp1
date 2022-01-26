53899f6ed84af84a9b79a6953107035402d5f124
Anna1 <Anna1@LAPTOP-RH0Q52CA>
2022-01-26 23:32:39 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DirsFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new TaskManager();
+            String Str = "Hello, World!";
+            String[] Str2 = { };
+            while (true)
+            {
+                Console.Write("console>>");
+                Str = Console.ReadLine();
+                if (Str == "help")
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("FindTask <название задачи>            поиск всех задач с введённым названием");
+                    Console.WriteLine("KillTaskById <ID>                     завершение задачи с введённым ID");
+                    Console.WriteLine("KillTaskByName <название задачи>      завершение всех задач с введённым названием (рекомендуется по возможности не использовать)");
+                    Console.WriteLine("PrintAllTasks                         вывод названий и ID всех активных задач");
+                    Console.WriteLine("end                                   завершение работы консоли");
+                    Console.WriteLine("help                                  получение справки о командах консоли");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    if (Str == null) { Str = "Hello, World!"; }
+                    Str2 = Str.Split(' ');
+                    if (Str2[0] == "FindTask" && Str2.Length > 1)
+                    {
+                        obj.FindTask(Str2[1]);
+                    }
+                    else if (Str2[0] == "KillTaskById" && Str2.Length > 1 && int.TryParse(Str2[1], out _))
+                    {
+                        obj.KillTaskById(Convert.ToInt32(Str2[1]));
+                    }
+                    else if (Str2[0] == "KillTaskByName" && Str2.Length > 1)
+                    {
+                        obj.KillTaskByName(Str2[1]);
+                    }
+                    else if (Str2[0] == "PrintAllTasks")
+                    {
+                        obj.PrintAllTasks();
+                    }
+                    else if (Str2[0] == "end")
+                    {
+                        obj.KillTaskByName("ConsoleApp1");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/TaskManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DirsFiles
+{
+    interface ITaskManager
+    {
+        public void PrintAllTasks();
+        public void KillTaskByName(string TaskName);
+        public void KillTaskById(int Id);
+        public void FindTask(string Str);
+    }
+    class TaskManager : ITaskManager
+    {
+        public void FindTask(string Str)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Найденные задачи:");
+            Console.WriteLine("Название                                            ID");
+            var task_list = Process.GetProcesses();
+            for (int i = 0; i < task_list.Length; i++)
+            {
+                if (task_list[i].ProcessName.ToLower().IndexOf(Str.ToLower()) >= 0)
+                {
+                    Console.WriteLine($"{task_list[i].ProcessName, 50}  {task_list[i].Id}");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public void KillTaskById(int Id)
+        {
+            var task_list = Process.GetProcesses();
+            for (int i = 0; i < task_list.Length; i++)
+            {
+                if (task_list[i].Id == Id)
+                {
+                    task_list[i].Kill();
+                }
+            }
+        }
+
+        public void KillTaskByName(string TaskName)
+        {
+            var task_list = Process.GetProcesses();
+            for (int i = 0; i < task_list.Length; i++)
+            {
+                if (task_list[i].ProcessName == TaskName)
+                {
+                    task_list[i].Kill();
+                }
+            }
+        }
+
+        public void PrintAllTasks()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Активные задачи задачи:");
+            Console.WriteLine("Название                                            ID");
+            var task_list = Process.GetProcesses();
+            for (int i=0; i < task_list.Length; i++)
+            {
+                Console.WriteLine($"{task_list[i].ProcessName, 50}  {task_list[i].Id}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
